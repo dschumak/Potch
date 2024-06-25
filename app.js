73c4023cc3299081a5088a1dcb63791c24fd92b8
Daniel Schumaker <danielschumaker@Daniels-Laptop.attlocal.net>
2024-06-25 14:17:54 -0400

    add helper function to retrieve a linear index from a two dimensional space

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ const TILE_SIZE = 16; // width of each square tile in pixels
             12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 
             51, 52, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 
             60, 60, 60, 52, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 
-            60, 60, 60, 60, 60, 52, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 
+            60, 60, 60, 60, 60, 52, 12, 12, 12, 12, 12, 12, 12, 0, 1, 1, 2, 12, 12, 12, 
             60, 60, 60, 60, 60, 59, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 
             12, 12, 23, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 
-            12, 12, 30, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 
-            12, 12, 37, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 
+            12, 12, 30, 12, 12, 12, 12, 12, 12, 12, 12, 12, 23, 12, 12, 12, 12, 12, 12, 12, 
+            12, 12, 37, 12, 12, 12, 12, 12, 12, 12, 12, 12, 37, 12, 12, 12, 12, 12, 12, 12, 
             1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
             8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 
         ],
@@ -82,21 +82,25 @@ async function splitTiles(filename) {
             let tile = new PIXI.Texture({
                 source: tileSet,
                 frame: frame
-            }); // loads all tiles and stores all of them into tile variable
-            tileTextures[y*xMax+x] = tile;
+            }); // loads all tiles and stores all of them into tile array
+            tileTextures[getIndex(x,y,xMax)] = tile;
         }
     }
     return tileTextures;
 }
 
-async function populateContainer(tileTextures, map, container) {
+function populateContainer(tileTextures, map, container) {
     for (let y=0; y<map.height; y++) {
         for (let x=0; x<map.width; x++) {
-            let tile = map.tiles[y*map.width+x]
+            let tile = map.tiles[getIndex(x,y,map.width)]
             let sprite = new PIXI.Sprite(tileTextures[tile]);
             sprite.x = x*TILE_SIZE;
             sprite.y = y*TILE_SIZE;
             container.addChild(sprite);
         }
     }
+}
+
+function getIndex(x, y, xMax) {
+    return y * xMax + x;
 }
